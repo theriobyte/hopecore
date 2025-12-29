decc6b4313367e9a800d15fe8c097a45652aa5ed
theriobyte <95988330+theriobyte@users.noreply.github.com>
2025-12-29 05:17:13 -0800

    adding ui

--- hope/Assets/Scripts/PlayerScript.cs
+++ hope/Assets/Scripts/PlayerScript.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    
+    private Rigidbody rb;
+    private Transform cam;
+
+    public int healthP = 5; // ?? add health
     public float speed = 9.5f;
     public float rotationSpeed = 2.0f;
     
-    public float horizontalInput;
-    public float verticalInput;
-    public float mouseXInput;
-    //public float mouseYInput;
+    private float horizontalInput;
+    private float verticalInput;
+    private float mouseXInput;
+    private float mouseYInput;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
@@ -29,8 +33,14 @@ public class PlayerScript : MonoBehaviour
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         mouseXInput = Input.GetAxis("Mouse X");
-        transform.Rotate(new Vector3(0, -mouseXInput * rotationSpeed, 0));
-        
+        transform.Rotate(new Vector3(0, mouseXInput * rotationSpeed, 0));
 
+        mouseYInput = Input.GetAxis("Mouse Y");
+        cam.transform.Rotate(new Vector3(-mouseYInput * rotationSpeed, 0, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0, 5, 0);
+        }
     }
 }
