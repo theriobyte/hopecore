53b45fc254e82c60f68ec0236cb97bb801f1b82e
theriobyte <95988330+theriobyte@users.noreply.github.com>
2025-12-29 20:57:01 -0800

    scene loader

--- hope/Assets/Scripts/GameManager.cs
+++ hope/Assets/Scripts/GameManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    /* useful
+     * 
+     * SceneManager.LoadScene("Game");    //By name
+     * SceneManager.LoadScene(1);         //By index
+     * 
+     */
     public PlayerScript pScript; //looks like pScript.boolvar
     // Start is called before the first frame update
     void Start()
@@ -16,4 +23,15 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
 }
