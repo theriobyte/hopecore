613c75ea9ed68dde695567f51a01a4a43fcb73f6
theriobyte <95988330+theriobyte@users.noreply.github.com>
2025-12-29 22:34:27 -0800

    jump fixed

--- hope/Assets/Scripts/PlayerScript.cs
+++ hope/Assets/Scripts/PlayerScript.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody rb;
+    private BoxCollider boxCollider;
     private Transform cam;
 
     public int healthP = 5; // ?? add health
     public float speed = 9.5f;
     public float rotationSpeed = 2.0f;
-    
+    public bool canJump = false;
+
     private float horizontalInput;
     private float verticalInput;
     private float mouseXInput;
@@ -38,9 +40,19 @@ public class PlayerScript : MonoBehaviour
         mouseYInput = Input.GetAxis("Mouse Y");
         cam.transform.Rotate(new Vector3(-mouseYInput * rotationSpeed, 0, 0));
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             rb.velocity = new Vector3(0, 5, 0);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Jumpable"))
+        {
+            //Debug.Log("canjump");
+            canJump = true;
         }
     }
 }
